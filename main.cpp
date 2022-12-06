f34f98b5411e9b8628db52545c8a1b7e6074720e
VirtualAI3 <elacaguate123>
2022-12-06 15:01:52 -0500

    Class citas

--- main.cpp
+++ main.cpp
@@ -3,53 +3,53 @@
 using namespace std;
 int main()
 {
-    Entry nameTime;
-    Time time(10, 30);
-    Date date;
-    date.Set(11, 7, 2022); // Establecer la fecha en 12 3 2004
-    Day day(date, 5);      // Establecer el dï¿½a en la fecha con citas
-    SortedList list;
-    for (int count = 1; count <= 3; count++)
-    {
-        nameTime.ReadEntry();
-        day.InsertEntry(nameTime);
-    }
-    // Preparar e iterar a travï¿½s de la lista, imprimir los elementos
-    int limit = day.NumberOfEntries();
-    day.ResetEntries(); // Preparar para iteraciï¿½n
-    cout << "Elementos para " << day.DateIs().Month() << " "
-         << day.DateIs().Day()
-         << ", " << day.DateIs().Year() << endl;
-    for (int count = 0; count < limit; count++)
-    {
-        nameTime = day.GetNextItem();
-        cout << "Nombre: " << nameTime.NameStr() << " Tiempo: "
-             << nameTime.TimeStr() << endl;
-    }
-    // El elemento estï¿½ en la lista
-    if (day.TimeFree(time))
-        cout << time.Hours() << ":" << time.Minutes()
-             << " esta libre." << endl;
-    else
-        cout << time.Hours() << ":" << time.Minutes()
-             << " no esta libre. " << endl;
-    // El elemento no estï¿½ en la lista
-    time.Set(0, 0);
-    if (day.TimeFree(time))
-        cout << time.Hours() << ":" << time.Minutes()
-             << " esta libre." << endl;
-    else
-        cout << time.Hours() << ":" << time.Minutes()
-             << " no esta libre. " << endl;
-    // Borrar e iterar a travï¿½s de la lista, imprimir los elementos
-    day.Delete(nameTime);
-    day.ResetEntries(); // Preparar para iteraciï¿½n
-    limit = day.NumberOfEntries();
-    for (int count = 0; count < limit; count++)
-    {
-        nameTime = day.GetNextItem();
-        cout << "Nombre: " << nameTime.NameStr() << " Tiempo: "
-             << nameTime.TimeStr() << endl;
-    }
-    return 0;
+ Entry nameTime;
+ Time time(10,30);
+ Date date;
+ date.Set(11, 7, 2022); // Establecer la fecha en 12 3 2004
+ Day day(date, 5); // Establecer el día en la fecha con citas
+ SortedList list;
+ for (int count = 1; count <= 3; count++)
+ {
+ nameTime.ReadEntry();
+ day.InsertEntry(nameTime);
+ }
+ // Preparar e iterar a través de la lista, imprimir los elementos
+ int limit = day.NumberOfEntries();
+ day.ResetEntries(); // Preparar para iteración
+ cout << "Elementos para " << day.DateIs().Month() << " "
+ << day.DateIs().Day()
+ << ", " << day.DateIs().Year() << endl;
+ for (int count = 0; count < limit; count++)
+ {
+ nameTime = day.GetNextItem();
+ cout << "Nombre: " << nameTime.NameStr() << " Tiempo: "
+ << nameTime.TimeStr() << endl;
+ }
+ // El elemento está en la lista
+ if (day.TimeFree(time))
+ cout << time.Hours() << ":" << time.Minutes()
+ << " esta libre." << endl;
+ else
+ cout << time.Hours() << ":" << time.Minutes()
+ << " no esta libre. " << endl;
+ // El elemento no está en la lista
+ time.Set(0,0);
+ if (day.TimeFree(time))
+ cout << time.Hours() << ":" << time.Minutes()
+ <<" esta libre." << endl;
+ else
+ cout << time.Hours() << ":" << time.Minutes()
+ << " no esta libre. " << endl;
+ // Borrar e iterar a través de la lista, imprimir los elementos
+ day.Delete(nameTime);
+ day.ResetEntries(); // Preparar para iteración
+ limit = day.NumberOfEntries();
+ for (int count = 0; count < limit; count++)
+ {
+ nameTime = day.GetNextItem();
+ cout << "Nombre: " << nameTime.NameStr() << " Tiempo: "
+ << nameTime.TimeStr() << endl;
+ }
+ return 0;
 }
